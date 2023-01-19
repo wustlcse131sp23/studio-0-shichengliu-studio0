35e9b3886320f45d542f1ba5b329e10464d49e9b
paradox <paradox@Prometheus.dhcp.wustl.edu>
2023-01-19 12:22:14 -0600

    hi

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,6 +4,6 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
-		System.out.println("Say hi to your group mates oi mate!");
+		System.out.println("Say hi to your group mates oi mate hihihihi!");
 	}
 }
